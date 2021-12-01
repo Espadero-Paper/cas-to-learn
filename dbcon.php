80b8232b9b65dfc11b2b4a6f17c2fa151b54baa4
Marlon Espadero <marlonespadero2@gmail.com>
2021-12-01 14:40:00 +0800

    Update dbcon.php

--- dbcon.php
+++ dbcon.php
@@ -1,5 +1,5 @@
 <?php
-$conn = mysqli_connect('remotemysql.com','3CTjHrjUyS','','gNOmy0VgtE');
+$conn = mysqli_connect('remotemysql.com','3CTjHrjUyS','gNOmy0VgtE','3CTjHrjUyS');
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
