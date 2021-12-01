93c6a607f70d7dd1e4269c76291eb22e71265f47
Marlon Espadero <marlonespadero2@gmail.com>
2021-12-01 14:11:50 +0800

    update db

--- dbConnector.php
+++ dbConnector.php
@@ -8,11 +8,15 @@ var $link;
     public function __construct() {
         ob_start();
   // Get the main settings from the array we just loaded
-        $host = 'localhost';
-        $db = 'capstone';
-        $user = 'root';
-        $pass = '';
+       // $host = 'localhost';
+       // $db = 'capstone';
+       // $user = 'root';
+        //$pass = '';
 
+        $host = 'remotemysql.com';
+        $db = '3CTjHrjUyS';
+        $user = '3CTjHrjUyS';
+        $pass = 'gNOmy0VgtE';
         // Connect to the database
         $this->link = mysqli_connect($host, $user, $pass,$db);
 
@@ -25,10 +29,10 @@ var $link;
 function DbConnector(){
 
         // Get the main settings from the array we just loaded
-        $host = 'localhost';
-        $db = 'capstone';
-        $user = 'root';
-        $pass = '';
+        $host = 'remotemysql.com';
+        $db = '3CTjHrjUyS';
+        $user = '3CTjHrjUyS';
+        $pass = 'gNOmy0VgtE';
 
         // Connect to the database
         $this->link = mysqli_connect($host, $user, $pass,$db);
