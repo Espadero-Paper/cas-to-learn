7d355de685f17e6c95e42c6cd82cc19fd5ecd337
Marlon Espadero <marlonespadero2@gmail.com>
2021-12-01 16:03:06 +0800

    Update dbcon.php

--- admin/dbcon.php
+++ admin/dbcon.php
@@ -1,3 +1,3 @@
 <?php
-$conn = mysqli_connect('localhost','root','','capstone') or die(mysqli_error());
+$conn = mysqli_connect('remotemysql.com','3CTjHrjUyS','gNOmy0VgtE','3CTjHrjUyS') or die(mysqli_error());
 ?>
