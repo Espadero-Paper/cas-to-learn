1f8e9cdc611f3130b2263ba222b040c704a4bc72
Marlon Espadero <marlonespadero2@gmail.com>
2021-12-01 15:25:04 +0800

    Update dbConnector.php

--- dbConnector.php
+++ dbConnector.php
@@ -12,8 +12,7 @@ var $link;
        // $db = 'capstone';
        // $user = 'root';
         //$pass = '';
-
-        $host = 'localhost';
+        $host = 'remotemysql.com';
         $db = '3CTjHrjUyS';
         $user = '3CTjHrjUyS';
         $pass = 'gNOmy0VgtE';
@@ -21,48 +20,6 @@ var $link;
         $this->link = mysqli_connect($host, $user, $pass,$db);
 
     }
-    function __destruct() {
-        // var_dump($this->link);
-        mysqli_close($this->link);
-        ob_end_flush();
-    }
-function DbConnector(){
-
-        // Get the main settings from the array we just loaded
-        $host = 'localhost';
-        $db = '3CTjHrjUyS';
-        $user = '3CTjHrjUyS';
-        $pass = 'gNOmy0VgtE';
-
-        // Connect to the database
-        $this->link = mysqli_connect($host, $user, $pass,$db);
-        // register_shutdown_function(array(&$this, 'close'));
-        return $this->link;
-
-    }
-	
-  //*** Function: query, Purpose: Execute a database query ***
-    function query($query) {
-
-        $this->theQuery = $query;
-        return mysqli_query($this->link,$query);
-
-    }
-
-    //*** Function: fetchArray, Purpose: Get array of query results ***
-    function fetchArray($result) {
-
-        return mysqli_fetch_array($result);
-
-    }
-
-    //*** Function: close, Purpose: Close the connection ***
-    function close() {
-
-        mysqli_close($this->link);
-
-    }
-	
 }
 
 ?>
