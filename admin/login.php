89f998808a5d6c21f5f1e401df908e200ccd8e90
Marlon Espadero <marlonespadero2@gmail.com>
2021-12-01 17:14:05 +0800

    Update login.php

--- admin/login.php
+++ admin/login.php
@@ -10,7 +10,8 @@
 		$username = $_POST['username'];
 		$password = $_POST['password'];
 
-		$query = mysqli_query($conn,"SELECT * FROM users WHERE username='$username' AND password = '$password'")or die(mysqli_error());
+		$query = mysqli_query($conn,"SELECT * FROM student WHERE username='$username' AND password='$password'";
+		$result = mysqli_query($conn,$query)or die(mysqli_error());
 		$count = mysqli_num_rows($query);
 		$row = mysqli_fetch_array($query);
 
@@ -21,7 +22,7 @@
 		
 		echo 'true';
 		
-		mysqli_query($conn,"insert into user_log (username,login_date,user_id)values('$username',NOW(),".$row['user_id'].")")or die(mysqli_error());
+		mysqli_query($conn,"insert into user_log(username,login_date,user_id)values('$username',NOW(),".$row['user_id'].")")or die(mysqli_error());
 		 }else{ 
 		echo 'false';
 		}	
